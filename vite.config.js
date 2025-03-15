9023dfafff94669698d9e754d70caa3a179468b3
LuhDevloper <seuemail@exemplo.com>
2025-03-14 21:34:52 -0300

    capetam

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Garante que os caminhos sejam relativos e funcionem na Vercel
   build: {
-    outDir: 'dist', // Gera a pasta 'dist' na raiz do projeto
-    emptyOutDir: true, // Limpa a pasta 'dist' antes de gerar novos arquivos
+    outDir: 'dist',
+    emptyOutDir: true,
   },
   css: {
     modules: {
-      localsConvention: 'camelCase', // Geração de classes com camelCase
+      localsConvention: 'camelCase',
     },
   },
 });
