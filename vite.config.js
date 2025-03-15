264396bb7eb7afd4ad600d3f9423931b566d4ede
LuhDevloper <seuemail@exemplo.com>
2025-03-14 22:20:41 -0300

    capeta

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src/frontend', // Configura o diretório de entrada para o frontend
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    outDir: '../dist', // Saída para o diretório 'dist' na raiz
+    emptyOutDir: true, // Limpa a pasta de saída antes de gerar os novos arquivos
   },
   css: {
     modules: {
-      localsConvention: 'camelCase',
+      localsConvention: 'camelCase', // Geração de classes CSS com camelCase
     },
   },
+  server: {
+    port: 3000, // Porta para o servidor de desenvolvimento
+  },
 });
-  
